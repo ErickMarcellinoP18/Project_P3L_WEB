f37aabd6548db2793178f0a161d67172ca82c88c
ErickMarcellinoP18 <resibebekeke5@gmail.com>
2024-04-15 20:47:28 +0700

    2

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homePage');
 });
+
+Route::get('/login', function () {
+    return view('loginPage');
+});
+
